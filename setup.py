f5e25ab7125eece8cc402e6c3e3b4cb171b34daa
Surat Teerapittayanon <steerapi@gmail.com>
2020-09-14 14:06:27 +0700

    chore: bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='rxnlvl',  
-     version='0.2',
+     version='0.3.0',
      scripts=['rxnlvl/__init__.py'] ,
      author="Surat Teerapittayanon",
      author_email="steerapi@gmail.com",
