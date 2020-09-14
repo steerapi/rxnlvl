63c77bfb9a2c1fe76494ca95eefcc0c3082de75e
Surat Teerapittayanon <steerapi@gmail.com>
2020-09-14 13:51:08 +0700

    fix: setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      name='rxnlvl',  
      version='0.1',
-     scripts=['rxnlvl'] ,
+     scripts=['rxnlvl/__init__.py'] ,
      author="Surat Teerapittayanon",
      author_email="steerapi@gmail.com",
      description="A simple python package for drawing attractive chemical reaction energy level diagrams.",
